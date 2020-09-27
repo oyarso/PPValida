0abe313cef3f3d97f82742d832db15a379d6c7c6
oyarso <38697226+oyarso@users.noreply.github.com>
2020-09-27 12:00:15 -0300

    Initial commit

---
+++ PPValidaWindowsService/PPValidaWindowsService/PPVWindowsService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Timers;
+using System.Configuration;
+
+
+namespace PPValidaWindowsService
+{
+    public partial class PPVWindowsService : ServiceBase
+    {
+        public PPVWindowsService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            EventLog.WriteEntry("PPValida Windows Service se esta iniciando", EventLogEntryType.Information);
+            string copiar = filecreate.ReadSetting("PathOrigen");
+            EventLog.WriteEntry(copiar, EventLogEntryType.Information);
+            FileSystemWatcher observador = new FileSystemWatcher(copiar);
+            observador.NotifyFilter = (
+            NotifyFilters.LastAccess |
+            NotifyFilters.LastWrite |
+            NotifyFilters.FileName |
+            NotifyFilters.DirectoryName);
+
+            observador.Filter = "*.txt";
+            try
+            {
+                observador.Created += filecreate.AlCambiar;
+                observador.EnableRaisingEvents = true;
+            }
+            catch
+            {
+                ServiceController sc = new ServiceController();
+                sc.ServiceName = "PPVWindowsService";
+
+                if (sc.Status == ServiceControllerStatus.Stopped)
+                {
+                    try
+                    {
+                        sc.Start();
+                        sc.WaitForStatus(ServiceControllerStatus.Running);
+                    }
+                    catch (InvalidOperationException)
+                    {
+
+                    }
+                }
+            }
+
+
+        }
+
+        protected override void OnStop()
+        {
+            EventLog.WriteEntry("PPValida Windows Service se esta deteniendo", EventLogEntryType.Information);
+        }
+    }
+
+
+}
+
---
+++ PPValidaWindowsService/PPValidaWindowsService/filecreate.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.ServiceProcess;
+using FileHelpers;
+using System.Configuration;
+
+
+
+namespace PPValidaWindowsService
+{
+    public class filecreate
+    {
+        public static string ReadSetting(string key)
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                string result = appSettings[key] ?? "Not Found";
+                return result;
+            }
+            catch (ConfigurationErrorsException)
+            {
+               return("no existe path");
+            }
+        }
+        public static void AlCambiar(object source, FileSystemEventArgs e)
+        {
+            string fileToCopy = e.FullPath.ToString();
+            string pegar = ReadSetting("PathDestino");
+            string copiar = ReadSetting("PathOrigen");
+            //string path = @"C:\home\global\log.txt";
+            //using (var tw = new StreamWriter(path, true))
+            //{
+            //    tw.WriteLine("pegar: "); tw.WriteLine(pegar);
+            //    tw.WriteLine("copiar: "); tw.WriteLine(copiar);
+            //}
+            string destinationDirectory = pegar;
+                try
+                {
+                    var engine = new FileHelperEngine<Documents>();
+                    var records = engine.ReadFile(e.FullPath);
+
+                    if (Documents.EsDoc(records[0].tipoDoc) && Documents.EsPrint(records[2].tipoDoc) && Documents.EsFecha(records[3].tipoDoc))
+                    {
+                    
+                    //using (var tw = new StreamWriter(path, true))
+                    //{
+                    //    tw.WriteLine("entra al document");
+                    //}
+                    try
+                        {
+                            File.Copy(fileToCopy, destinationDirectory + Path.GetFileName(fileToCopy));
+                        }
+                        catch
+                        {
+                            ServiceController sc = new ServiceController();
+                            sc.ServiceName = "PPVWindowsService";
+
+                            if (sc.Status == ServiceControllerStatus.Stopped)
+                            {
+                                // Start the service if the current status is stopped.
+                                try
+                                {
+                                    // Start the service, and wait until its status is "Running".
+                                    sc.Start();
+                                    sc.WaitForStatus(ServiceControllerStatus.Running);
+                                    // Display the current service status.
+                                }
+                                catch (InvalidOperationException)
+                                {
+                                }
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    ServiceController sc = new ServiceController();
+                    sc.ServiceName = "PPVWindowsService";
+
+                    if (sc.Status == ServiceControllerStatus.Stopped)
+                    {
+                        // Start the service if the current status is stopped.
+                        try
+                        {
+                            // Start the service, and wait until its status is "Running".
+                            sc.Start();
+                            sc.WaitForStatus(ServiceControllerStatus.Running);
+                            // Display the current service status.
+                        }
+                        catch (InvalidOperationException)
+                        {
+                        }
+                    }
+                }
+            }
+    }
+}
